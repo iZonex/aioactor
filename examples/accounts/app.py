79f4165928d63d0ecd8f71604b5b18898a972e38
Dmytro Chystiakov <dlchistyakov@gmail.com>
2018-01-07 19:03:57 +0200

    [accounts] add TODOs list

--- examples/accounts/app.py
+++ examples/accounts/app.py
@@ -4,6 +4,7 @@ from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 # TODO ADD possible actions list!
+# TODO ADD abstractions to Message Handler!
 
 class ServiceBroker:
 
